09ca5b970ee48f0b207b15c47778c49c9914a2a4
oasis0001 <osaman2581@gmail.com>
2026-02-24 16:35:24 +0530

    Clean commit after removing large asset

---
+++ 01_basics/01_Basics.js
@@ -0,0 +1,174 @@
+/* Var and Const and Let */
+
+/* const accountID = 1234323
+ let accountEmail = "osaman2581@gmail.com"
+ var accountPass = "123123"
+ accountCity = "Hyderabad"
+ let accountState = "Telengana";  
+ console.table([accountEmail, accountID, accountPass, accountCity, accountState])
+*/
+/* Data Conversion */
+
+/* let score = "33abc"
+console.log(typeof score);
+let valueInNum = Number(score)
+console.log(valueInNum); 
+*/
+
+/* let isLoggedin = "Aman"
+let booleanIsLogginIn = Boolean(isLoggedin)
+console.log(booleanIsLogginIn); */
+
+/* let nummber = 1234
+let stringnum = String(nummber)
+console.log(typeof stringnum);
+ */
+
+
+/* Operations */
+
+
+/* let value = 3
+let negValue = -value
+let str1= "hello"
+let str2 = "Aman"
+let num1 = "3"
+let num2 = "12"
+let str3 = str1 + str2
+
+console.log(2+3);
+console.log(2-3);
+console.log(12/3);
+console.log(12*3);
+console.log(2**3);
+console.log(11%3);
+console.log(str3);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(3 + true);
+console.log(3 + 1 + "2");
+console.log("2" + 3 + 1) */
+
+/* console.log(true+false);
+console.log(true+true);
+console.log(false+false);
+console.log(false+true);
+ */
+
+/* let gamecounter1 = 100
+let gamecounter2 = 100
+let gamecounter3 = 100
+let gamecounter4 = 100
+gamecounter1++
+console.log(gamecounter1);
+gamecounter2--
+console.log(gamecounter2);
+--gamecounter3
+console.log(gamecounter3);
+++gamecounter4
+console.log(gamecounter4); */
+
+/* console.log("12" >> 2);
+console.log("01" == 1);
+console.log(2 === "2");
+console.log(2 !== "2"); */
+
+
+/* const person = {
+id: 1,
+name: 'John',
+age: 25,
+}
+const dog = {
+name: 'puppy',
+personid: 1,
+}
+
+function getOwner(dog, persons) {
+return persons.find((person) => person.id === dog.personid)
+} */
+
+/* console.log(person);
+console.log(dog);
+console.log(getOwner);
+console.log(getOwner(dog, [person])); */
+
+const name = "Aman"
+console.log(`Hello I am ${name}, nice to meet you!`);
+console.log(name.length);
+console.log(name.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Arrays and Objects */
+
+/* let myArr = Array("Aman", "Sahil", "Alok", "Rohit")
+console.log(myArr);
+console.log(myArr[2]);
+myArr[2] = "Changed Sahil"
+console.log(myArr);
+ */
+
+/* const heroes = ["Batman", "Superman", "Spiderman", "Ironman"];
+console.log(heroes.includes("Spiderman"));
+console.log(!heroes.includes("Flash"));
+console.log(!heroes.every(soap => soap === "Flash"));
+console.log(heroes.some(soap => soap.toLowerCase() === "spiderman"))
+console.log(heroes.some(soap => soap.toLowerCase() === "spiderman"))
+console.log(heroes.some(soap => soap.startsWith("S")));
+console.log(heroes.every(soap => soap === "Batman")); */
+
+
+/* | Code       | Meaning              |
+| ---------- | -------------------- |
+| `some()`   | At least one matches |
+| `!some()`  | None match           |
+| `every()`  | All match            |
+| `!every()` | Not all match        |
+ */
+
+
+/* let myObj = {
+    name: "Aman",
+    age: 21,
+    hobbies: ["coding", "reading", "gaming"],
+    isLoggedIn: true,
+}
+console.log(myObj);
+console.log(myObj.age);
+myObj.age = 22
+console.log(myObj);
+console.log(myObj.age);
+ */
+
+
+
